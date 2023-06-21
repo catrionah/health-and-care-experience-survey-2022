add86388cadd6dfe2ca8f99b3b7257b0025c8fa4
catrionah <70889831+catrionah@users.noreply.github.com>
2023-06-21 13:16:46 +0100

    Add files via upload

---
+++ setup_files_paths.R
@@ -0,0 +1,29 @@
+# Name of file: HACE 2022 setup_file_paths.R
+# 
+# Data release (if applicable)
+# 
+# Original author(s): Catriona Haddow
+#   
+# Written/run on: Posit Workbench - RStudio R 4.1.2
+# 
+# Description of content: Create weight1 - probability of non-selection
+# 
+# Approximate run time: <1 min
+# 
+# Approximate memory usage: 245mib
+
+#Inputs:
+#"output/weights/eligible_pats_by_gp.rds"
+#"output/weights/sample_size_by_gp.rds"
+
+#Outputs:
+#"output/weights/weight1.rds"
+
+lookup_path   <- "/conf/bss/pat-exp-surveys/health-and-care/training/lookups/"
+data_path     <- "/conf/bss/pat-exp-surveys/health-and-care/training/data/"
+output_path   <- "/conf/bss/pat-exp-surveys/health-and-care/training/output/"
+
+
+
+
+
